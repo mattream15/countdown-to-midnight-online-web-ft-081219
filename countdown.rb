26e028ef4392d0bc11087ea33403021f771ef733
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-20 19:52:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+end
+ "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(num_secs)
+  while num_secs > 0
+    puts "#{num_secs} SECOND(S)!"
+  sleep(1)
+  num_secs -=1
+end
+"HAPPY NEW YEAR!"
 end
